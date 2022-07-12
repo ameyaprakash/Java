50d2dee957bbdb95403aa0f097bfd301aeaff8b9
ameyaprakash <95076013+ameyaprakash@users.noreply.github.com>
2022-07-12 21:27:59 +0530

    Add files via upload
    CMPLT

---
+++ CO4/Area.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import Graphics.Figure;
+class Area
+{
+	public static void main (String a[])
+	{
+		Figure k = new Figure();
+		k.circle();
+		k.rectangle();
+		k.triangle();
+		k.square();
+	}
+}
---
+++ CO4/Figure.java
@@ -0,0 +1,46 @@
+package Graphics;
+import java.util.*;
+interface FindArea
+{
+	public void circle();
+	public void square();
+	public void rectangle();
+	public void triangle();
+}
+public class Figure implements FindArea
+{
+	public void circle()
+	{
+		Scanner c = new Scanner(System.in);
+		System.out.println("enter radius of circle =");
+		int r = c.nextInt();
+		double a = 2*3.14*(r*r);
+		System.out.println("Area = "+a);
+	}
+	public void square()
+	{
+		Scanner c = new Scanner(System.in);
+		System.out.println("enter side of square=");
+		int r = c.nextInt();
+		double a = r*r;
+		System.out.println("Area = "+a);
+	}
+	public void rectangle()
+	{
+		Scanner c = new Scanner(System.in);
+		System.out.println("enter legth and breadth of rectangle =");
+		int r = c.nextInt();
+		int b =c.nextInt();
+		double a = r*b;
+		System.out.println("Area = "+a);
+	}
+	public void triangle()
+	{
+		Scanner c = new Scanner(System.in);
+		System.out.println("enter legth and breadth of triangle =");
+		int r = c.nextInt();
+		int b = c.nextInt();
+		double a = (b*r)/2;
+		System.out.println("Area = "+a);
+	}
+} 
